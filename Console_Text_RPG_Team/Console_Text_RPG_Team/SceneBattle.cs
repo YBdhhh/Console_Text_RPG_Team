6dae8c25fd587f763c4bdbea38ae5ac6c61bc505
YBdhhh <97926god@gmail.com>
2025-04-21 21:19:23 +0900

    Update: SceneBattle.cs
    SceneBattle 스크립트 나머지 구현 업데이트

--- Console_Text_RPG_Team/Console_Text_RPG_Team/SceneBattle.cs
+++ Console_Text_RPG_Team/Console_Text_RPG_Team/SceneBattle.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +13,22 @@ namespace Console_Text_RPG_Team
 
         public void BattleStart()
         {
+            StringBuilder sb = new StringBuilder();
+            Player player = new Player();
+
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("Battle!!");
+            Console.WriteLine("Battle!!\n");
             Console.ResetColor();
             MonsterSpawn();
+            sb.AppendLine("[내정보]");
+            sb.AppendLine($"Lv.	: {player.level}");
+            sb.AppendLine($"직업	: {player.job}");
+            sb.AppendLine($"체  력	: {player.hp}/{player.hp}");
+            sb.AppendLine("1. 공격").Append("\n");
+            sb.AppendLine("원하시는 행동을 입력해주세요.");
+            sb.Append(">> ");
+            Console.Write(sb.ToString());
+            InputAttack();
 
 
         }
@@ -46,6 +60,33 @@ namespace Console_Text_RPG_Team
                     default:
                         break;
                 }
+            }
+            Console.WriteLine();
+        }
+
+        public void InputAttack()
+        {
+            while (true)
+            {
+                try
+                {
+                    int input = int.Parse(Console.ReadLine());
+                    switch (input)
+                    {
+                        case 1:
+                            Console.WriteLine("공격창"); // SceneBattleAttack
+                            return;
+                        default:
+                            Console.WriteLine("다시 입력해주십시오");
+                            continue;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("다시 입력해주십시오");
+                    continue;
+                }
+
             }
         }
     }
