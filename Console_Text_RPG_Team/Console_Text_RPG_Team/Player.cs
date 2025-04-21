e66d8e0fedb4fdb209f448cfcfaf9a2323933781
Renew023 <ya1539@naver.com>
2025-04-21 16:45:02 +0900

    Init 객체 파일
    + Player와 Monster의 클래스 파일을 생성했습니다.

---
+++ Console_Text_RPG_Team/Console_Text_RPG_Team/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Text_RPG_Team
+{
+	internal class Player
+	{
+		public string name;
+		public string job;
+
+		public float hp = 100;
+		public float atk = 10;
+		public float def = 5;
+		public int gold = 1500;
+		public int level = 1;
+		
+
+
+	}
+}
---
+++ Console_Text_RPG_Team/Console_Text_RPG_Team/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Text_RPG_Team
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
