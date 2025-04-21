c43d553af6d8073e99fd133f26fc2fe42f683729
Renew023 <ya1539@naver.com>
2025-04-21 17:39:36 +0900

    feat. 직업 스크립트 추가
    + 직업 객체 스크립트 추가했습니다.

---
+++ Console_Text_RPG_Team/Console_Text_RPG_Team/Job.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Text_RPG_Team
+{
+	internal class Job
+	{
+		public string job;
+		public int hp;
+		public int atk;
+		public int def;
+
+		public Job(string job, int hp, int atk, int def)
+		{
+			this.job = job;
+			this.hp = hp;
+			this.atk = atk;
+			this.def = def;
+		}
+	}
+}
