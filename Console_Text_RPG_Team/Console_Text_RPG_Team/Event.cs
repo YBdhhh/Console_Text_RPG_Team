5109b19d032d53747c5a1b4eafe44deeb8d4a67b
Renew023 <ya1539@naver.com>
2025-04-23 19:02:11 +0900

    feat. Quest 구현 완료!
    + Quest 구현

--- Console_Text_RPG_Team/Console_Text_RPG_Team/Event.cs
+++ Console_Text_RPG_Team/Console_Text_RPG_Team/Event.cs
@@ -4,19 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+/*
 namespace Console_Text_RPG_Team
 {
-	internal class Event
+	internal class EventManager
 	{
+		private static EventManager eventManager;
+
 		//이벤트 조건
 		//공격 데미지
 		//현재 돈 
-		public int HitDamageCount;
-		public int KillCount; //Compare(==)
+		public int hitDamage;
+		public int killMob; //Compare(==)
+		public int killBoss;
+		public int useGold;
+		public int stack;
+		public string mission;
+
 		public int AttackDamageCount;
 		public int CulMoneyCount;
 		public int CulHp;
 		public int CulAtk; //현재 체력 
 		public int GetItem;
+		
+
+		
 	}
 }
+*/
