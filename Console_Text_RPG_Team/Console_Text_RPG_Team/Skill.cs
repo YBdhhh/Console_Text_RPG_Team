a9bc578d5ec5ba864b0cdf59254978e4ba1e9e1e
Renew023 <ya1539@naver.com>
2025-04-23 09:43:03 +0900

    feat. 스킬 스크립트 추가
    feat. 스킬 스크립트 추가

---
+++ Console_Text_RPG_Team/Console_Text_RPG_Team/Skill.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum DamageType
+{
+	Hp = 3,
+	Def = 6,
+	Atk = 12,
+	Mp = 24
+}
+
+namespace Console_Text_RPG_Team
+{
+	internal class Skill
+	{
+		public string name;
+		public string explain;
+		public int[] damage = new int[4];
+		public int damageType;
+		public int valueMp;
+
+		public int price;
+
+
+		public void SkillDamage(int damageType)
+		{
+			
+
+
+		}
+	}
+}
